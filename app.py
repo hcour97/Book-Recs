2218f43a240a3c35ce51094c306d29e1a2133170
Hannah <hcour@gmail.com>
2023-12-12 10:46:54 -0800

    add logout login feature

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ from flask import Flask, render_template, request, flash, redirect, session, g, 
 from sqlalchemy.exc import IntegrityError
 import pdb
 
-from forms import AddBookForm, BookRecommendationForm
-from models import db, connect_db, Book
+from forms import AddBookForm, BookRecommendationForm, UserLoginForm, UserAddForm
+from models import db, connect_db, Book, User
 
 CURR_USER_KEY = "curr_user"
-# https://openlibrary.org/search.json?q=the+lord+of+the+rings
+
 BASE_URL = "https://openlibrary.org/search.json?"
 
 app = Flask(__name__)
@@ -25,6 +25,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 # toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
+# db.drop_all()
 db.create_all()
 
 ########################################
@@ -63,94 +64,188 @@ def get_books(subject_name):
 
 ################## USE Flask's g object to store data during running web app #########
 
-# @app.before_request
-# def add_user_to_g():
-#     """If logged in, add current user to Flask Global."""
-#     if CURR_USER_KEY in session:
-#         g.user = User.query.get(session[CURR_USER_KEY])
+@app.before_request
+def add_user_to_g():
+    """If logged in, add current user to Flask Global."""
+    if CURR_USER_KEY in session:
+        g.user = User.query.get(session[CURR_USER_KEY])
 
-# def do_login(user):
-#     """login user."""
-#     session[CURR_USER_KEY] = user.id
+    else:
+        g.user = None
 
-# def do_logout():
-#     if CURR_USER_KEY in session:
-#         del session[CURR_USER_KEY]
+def do_login(user):
+    """login user."""
+    session[CURR_USER_KEY] = user.id
+
+def do_logout():
+    if CURR_USER_KEY in session:
+        del session[CURR_USER_KEY]
+
 ########################################
 ################# USER #################
 ################ ROUTES ################
 ########################################
 
-# @app.route("/signup", methods=["GET", "POST"])
-# def signup():
-#     """Handle new user signup. Create a new User and add to db. 
-#         Redirect to their reading list."""
-#     form = UserAddForm()
-#     if form.validate_on_submit():
-#         try: 
-#             user = User.signup(
-#                 username = form.username.data,
-#                 password = form.password.data,
-#                 email=form.password.data,
-#             )
-#             db.session.commit()
-#         except IntegrityError:
-#             flash("Username already taken", "danger")
-#             return render_template('users/signup.html', form=form)
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    """Handle new user signup. Create a new User and add to db. 
+        Redirect to their reading list."""
+    form = UserAddForm()
+    if form.validate_on_submit():
+        try: 
+            user = User.signup(
+                username = form.username.data,
+                password = form.password.data,
+                email=form.password.data,
+            )
+            db.session.commit()
+        except IntegrityError:
+            flash("Username already taken", "danger")
+            return render_template('users/signup.html', form=form)
         
-#         do_login(user)
-#         return redirect("/")
-#     else:
-#         return render_template('users/signup.html', form=form)
+        do_login(user)
+        return redirect("/")
+    else:
+        return render_template('users/signup.html', form=form)
  
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     """Handle existing user login."""
-#     form = UserLoginForm()
-#     if form.validate_on_submit():
-#         user=User.authenticate(form.username.data, form.password.data)
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Handle existing user login."""
+    form = UserLoginForm()
+    if form.validate_on_submit():
+        user=User.authenticate(form.username.data, form.password.data)
 
-#         if user:
-#             do_login(user)
-#             flash(f"Hello, {user.username}!", "success")
-#             return redirect("/")
+        if user:
+            do_login(user)
+            flash(f"Hello, {user.username}!", "success")
+            return redirect("/")
         
-#         else:
-#             flash("Invalid credentials. Please try again.", "danger")
+        else:
+            flash("Invalid credentials. Please try again.", "danger")
     
-#     return render_template("users/login.html", form=form)
+    return render_template("users/login.html", form=form)
 
-# @app.route("/logout", methods=["GET", "POST"])
-# def logout():
-#     """Handle existing user logout."""
-#     do_logout()
-#     flash("User successfully logged out.")
-#     return redirect("/login")
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    """Handle existing user logout."""
+    do_logout()
+    flash("User successfully logged out.")
+    return redirect("/login")
 
 ########################################
 ################ ROUTES ################
 ########################################
 
-@app.route("/", methods=["GET"])
-def homepage():
-    """Homepage that displays books the user would like to read."""
-    books = Book.query.all()
-    return render_template("users_books.html", books=books)
+@app.route("/book-recs/add", methods=["GET", "POST"])
+def manually_add_book():
+    """Allows logged-in user to manually add a book to their list."""
 
-@app.route("/form", methods=["GET", "POST"])
-def view_books():
-    """Form for user to add books to their read-list."""
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
     form = AddBookForm()
     if form.validate_on_submit():
         book = Book(title=form.title.data) # create with Book model
+        g.user.books.append(book) # add book to g.user session
 
-        db.session.add(book) # add book to session
+        # db.session.add(book) # add book to session
         db.session.commit() # add to db
         
         ## not working
         flash(f"Success, ${book.title} was added to your list.", 'success') 
-        return redirect('/')
+        return redirect("/users/{g.user.id}")
     return render_template("add_book_form.html", form=form)
+
+@app.route("/book-recs/new", methods = ["GET", "POST"])
+def add_book_recs():
+    """Get a new list of books recs.
+    Show form if GET. If valid, update message and redirect to results page."""
+
+    if not g.user:
+        flash("Access unauthorized", "danger")
+        return redirect("/") ### figure out what this new "/" page will look like...
+    
+    form = BookRecommendationForm()
+
+    if form.validate_on_submit():
+        book1 = form.title1.data
+        book2 = form.title2.data
+        subj1 = get_subject(book1)
+        subj2 = get_subject(book2)
+    
+        try:
+            common_subjects = get_common_subjects(subj1, subj2)
+            subject = common_subjects[0]
+            query_subject = common_subjects[0].lower().replace(' ', '+')
+
+            resp = requests.get(f"https://openlibrary.org/search.json?subject={query_subject}")
+            info = resp.json()
+        
+            books = []
+            i = 0
+            while i <=5:
+                book_title = info['docs'][i]['title']
+                # book = Book(title=book_title.data) ## not actually saving to the db.
+                books.append(book_title)
+                i+=1
+            # return redirect("/book-recs/add")
+            return render_template("recommendation_results.html", form=form, books=books, subject=subject)
+        except:
+            return render_template("error_recommendations.html")
+    return render_template("recommendation_form.html", form=form)
+
+@app.route("/users/<int:user_id>", methods=["GET"])
+def show_books(user_id):
+    """Display the users to-read list."""
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+    
+    user = User.query.get_or_404(user_id)
+    books = user.books
+    return render_template("users/books.html", user=user, books=books)
+
+@app.route("/")
+def homepage():
+    """Show homepage
+    anon users: no books
+    logged in users: book list."""
+
+    if g.user: 
+        books = Book.query.all()
+        # user = user.query.get_or_404(user_id)
+        return render_template("homepage.html", books=books)
+    
+    else:
+        return render_template("home-anon.html")
+
+
+
+
+
+##### PREVIOUS ROUTES #####
+
+# @app.route("/", methods=["GET"])
+# def homepage():
+#     """Homepage that displays books the user would like to read."""
+#     books = Book.query.all()
+#     return render_template("users_books.html", books=books)
+
+# @app.route("/form", methods=["GET", "POST"])
+# def view_books():
+#     """Form for user to manually add books to their read-list."""
+#     form = AddBookForm()
+#     if form.validate_on_submit():
+#         book = Book(title=form.title.data) # create with Book model
+
+#         db.session.add(book) # add book to session
+#         db.session.commit() # add to db
+        
+#         ## not working
+#         flash(f"Success, ${book.title} was added to your list.", 'success') 
+#         return redirect('/')
+#     return render_template("add_book_form.html", form=form)
 
 @app.route('/delete/<int:book_id>', methods=["GET","POST"])
 def delete_book(book_id):
